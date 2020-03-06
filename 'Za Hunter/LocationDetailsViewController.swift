426111e5eb346d6a463086eb7a594bf5f0624bce
Ryan Bachrach <ryanbachrach9@gmail.com>
2020-03-06 11:00:09 -0600

    finished app :)

--- 'Za Hunter/LocationDetailsViewController.swift
+++ 'Za Hunter/LocationDetailsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LocationsDetailsViewController.swift
+//  LocationDetailsViewController.swift
 //  'Za Hunter
 //
 //  Created by ryan bachrach on 3/4/20.
@@ -8,16 +8,41 @@
 
 import UIKit
 import MapKit
+import SafariServices
 
 class LocationDetailsViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
     var selectedMapItem = MKMapItem()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         print(selectedMapItem.name!)
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        nameLabel.text = selectedMapItem.placemark.name
+        var address = selectedMapItem.placemark.subThoroughfare! + " "
+        address += selectedMapItem.placemark.thoroughfare! + "\n"
+        address += selectedMapItem.placemark.locality! + ", "
+        address += selectedMapItem.placemark.administrativeArea! + " "
+        address += selectedMapItem.placemark.postalCode!
+        addressLabel.text = address
+        phoneLabel.text = selectedMapItem.phoneNumber
+    }
+    
+    @IBAction func onDirectionsButtonTapped(_ sender: Any) {
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking]
+        MKMapItem.openMaps(with: [selectedMapItem], launchOptions: launchOptions)
+    }
+    
+    @IBAction func onWebsiteButtonTapped(_ sender: Any) {
+        if let url = selectedMapItem.url {
+           present(SFSafariViewController(url: url), animated: true)
+        }
     }
     
 }
